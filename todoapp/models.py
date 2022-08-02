4551f83188db10f5b87e07dbfaa92c02dd6600e6
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-08-02 13:19:03 +0300

    custom user model refactoring and create api

--- todoapp/models.py
+++ todoapp/models.py
@@ -17,6 +17,8 @@ class CustomUserModel(AbstractUser):
 
     class Meta:
         db_table = "users"
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
     def __str__(self):
-        return
+        return self.email
