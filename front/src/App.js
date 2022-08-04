7724584ffa408cd8a75612f3508fdc8d2b41f0a0
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-08-04 21:17:10 +0300

    create react app

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Редактируйте <code>src/App.js</code> и сохраните для перезагрузки.
+                </p>
+                <a className="App-link"
+                   href="https://ru.reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
