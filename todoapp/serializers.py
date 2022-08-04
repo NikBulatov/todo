76e621e6c0ea39f2a4ee408145abfbb595f95ee4
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-08-04 21:58:49 +0300

    reduce username field

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -5,4 +5,4 @@ from todoapp.models import CustomUserModel as User
 class UserBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email')
