3d1e9073d43b66fa5760027cfa60d6effbe5dc8d
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-09-19 21:08:44 +0300

    add ID field to user

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -6,7 +6,7 @@ from todoapp.models import CustomUserModel as User, Project, ToDo
 class UserBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 
 class UserSerializerVersion01(UserBaseSerializer):
